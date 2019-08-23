1b8712b8e262cd610ac766e29055434dfde9a02a
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-08-23 15:01:27 +0200

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { Quote } from './quote';
+// import { Component } from '@angular/core';
+// import { Quote } from './quote';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
   // title = 'Quotes';
   // quotes: Quote[] = [
   //   { id: 1, quote: 'Reading Is To The Mind, As Exercise Is To The Body.', author: 'Brian Tracy' },
   //   { id: 2, quote: 'Today’s Accomplishments Were Yesterday’s Impossibilities.', author: 'Robert H. Schuller' },
   // ];
-  constructor() { }
-}
+//   constructor() { }
+// }
